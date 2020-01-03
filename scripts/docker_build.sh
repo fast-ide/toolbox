0d432a0d8831294655fa66483e0981bd731fbf5e
rusdevops <rusdevops@gmail.com>
2020-01-03 21:19:49 +0300

    fixed typo

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -4,4 +4,4 @@ pushd linuxbrew/docker/${OS_FAMILY}-${OS_VERSION}
 tar -czh . | docker build -t fastide/linuxbrew-${OS_FAMILY}:${OS_VERSION} -
 popd
 cd docker/${OS_FAMILY}-${OS_VERSION}
-tar -czh . | docker build -t fastide/bootstap-${OS_FAMILY}:${OS_VERSION} -
+tar -czh . | docker build -t fastide/bootstrap-${OS_FAMILY}:${OS_VERSION} -
