111704ed1528745e19b8965b1d6006598829caad
rusdevops <rusdevops@gmail.com>
2020-03-23 09:14:54 +0300

    added fedora-30 and fix node installation for macos

--- requirements.sh
+++ requirements.sh
@@ -11,6 +11,13 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
     ;;
   darwin*)
     which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # https://stackoverflow.com/posts/21520777/timeline
+    brew uninstall npm
+    brew uninstall node
+    npm uninstall npm -g
+    sudo rm -rf /usr/local/lib/node_modules
+    sudo rm -rf /usr/local/include/node
+    sudo rm -rf /usr/local/lib/node
     ;;
   *)
     echo "unsupported OS"
