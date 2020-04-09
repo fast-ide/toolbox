fb5132b7055c62d24a1a3d2a58f360587fc1f6a3
rusdevops <rusdevops@gmail.com>
2020-04-09 12:34:28 +0300

    added lf for integration fast-ide with file manager

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ brew install --build-bottle ag \
                             httpie \
                             jid \
                             jq \
+                            lf \
                             ncdu \
                             neovim \
                             node \
