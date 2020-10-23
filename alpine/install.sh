01e93f1783df4b688846eae7c69678466491d799
rusdevops <rusdevops@gmail.com>
2020-10-23 09:10:05 +0300

    Update install.sh

--- alpine/install.sh
+++ alpine/install.sh
@@ -32,7 +32,7 @@ ln -s /usr/bin/python3 /usr/bin/python
 
 apk add python3-dev libc-dev
 
-pip3 install pynvim git-plus
+python -m pip install pynvim git-plus
 
 wget https://github.com/gokcehan/lf/releases/download/r14/lf-linux-amd64.tar.gz && \
 tar -xf lf-linux-amd64.tar.gz && \
